f2169961e4331bda9771c82b3d7875863e137f92
Garion Cox <garion.cox@students.snow.edu>
2023-11-09 11:59:46 -0700

    Checkpoint for displaying image from database

--- ArtCommissions/Program.cs
+++ ArtCommissions/Program.cs
@@ -1,13 +1,14 @@
 using ArtCommissions.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-//builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContextFactory<PostgresContext>(config => config.UseNpgsql(builder.Configuration.GetConnectionString("db")));
 
 var app = builder.Build();
 
