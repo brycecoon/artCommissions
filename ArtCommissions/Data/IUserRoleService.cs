93f308ab6dc57aa653f2d829aaf202105de9af64
Garion Cox <garion.cox@students.snow.edu>
2023-11-15 11:30:04 -0700

    Added IUserRoleService and implementation from class

---
+++ ArtCommissions/Data/IUserRoleService.cs
@@ -0,0 +1,9 @@
+﻿namespace ArtCommissions.Data;
+
+public interface IUserRoleService
+{
+    public bool IsAuthenticated { get; }
+    public IEnumerable<string> Roles { get; }
+    public Task LookUpUser(string email);
+    public void ResetUser();
+}
---
+++ ArtCommissions/Data/Services/UserRolesService.cs
@@ -0,0 +1,23 @@
+﻿namespace ArtCommissions.Data.Services;
+
+public class UserRolesService : IUserRoleService
+{
+    public bool IsAuthenticated { get; private set; }
+    public IEnumerable<string> Roles => _roles;
+    private List<string> _roles = new();
+    private PostgresContext dbcontext;
+    public UserRolesService(PostgresContext context)
+    {
+        dbcontext = context;
+    }
+
+    public Task LookUpUser(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ResetUser()
+    {
+        throw new NotImplementedException();
+    }
+}
